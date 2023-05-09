c04d91f12539778e50387ff9d2fbb78964f54c00
MidnightTale <midnighttale24@gmail.com>
2023-05-09 20:45:59 +0700

    Initial commit

---
+++ src/main/java/xyz/hynse/hynseapi/HynseAPI.java
@@ -0,0 +1,17 @@
+package xyz.hynse.hynseapi;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HynseAPI extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
